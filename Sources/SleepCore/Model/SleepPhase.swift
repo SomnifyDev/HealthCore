b396f0649a219d1018c1ece0a39bdc3bfecedf8b
Nikita Kazantsev <kaznik2013@gmail.com>
2022-04-05 22:17:39 +0300

    sleep provider changes

--- Sources/SleepCore/Model/SleepPhase.swift
+++ Sources/SleepCore/Model/SleepPhase.swift
@@ -4,11 +4,19 @@ import Foundation
 
 public typealias SampleData = (date: Date, value: Double)
 
+public enum Condition {
+    case awake
+    case light
+    case deep
+}
+
 // MARK: - SleepPhase
+
 
 public struct SleepPhase {
     
-    // TODO: - Implement SleepPhases as it should be
+    public let interval: DateInterval
+    public let condition: Condition
     public let heartData: [SampleData]
     public let energyData: [SampleData]
     public let respiratoryData: [SampleData]
--- Sources/SleepCore/SleepCoreProvider.swift
+++ Sources/SleepCore/SleepCoreProvider.swift
@@ -35,9 +35,10 @@ public final class SleepCoreProvider: ObservableObject {
     }
 
     // MARK: - Public methods
-    
-    public func retrieveLastSleep(
-        author: HealthCoreProvider.BundleAuthor = .concrete(identifiers: ["com.apple"])
+
+    public func detectLastSleep(
+        author: HealthCoreProvider.BundleAuthor = .concrete(identifiers: ["com.apple"]),
+        shouldSaveToHealthKit: Bool
     ) async throws -> Sleep {
         self.lock.lock()
         let currentDate = Date()
@@ -106,10 +107,13 @@ public final class SleepCoreProvider: ObservableObject {
                     continue
                 }
 
-                let _ = energySamples
-                let _ = heartSamples
-                let _ = respiratorySamples
-                let sleepPhases: [SleepPhase] = [] // TODO: - Implement phases detection
+                let sleepPhases: [SleepPhase] = PhasesComputationService.computatePhases(
+                    energySamples: energySamples ?? [],
+                    heartSamples: heartSamples ?? [],
+                    breathSamples: respiratorySamples ?? [],
+                    sleepInterval: asleepInterval
+                )
+
                 let microSleep = MicroSleep(
                     sleepInterval: asleepInterval,
                     inBedInterval: inbedInterval,
@@ -118,7 +122,10 @@ public final class SleepCoreProvider: ObservableObject {
 
                 sleep.samples.append(microSleep)
                 isFirstFetch = false
-                try await self.saveMicroSleepIfNeeded(microSleep: microSleep)
+
+                if shouldSaveToHealthKit {
+                    try await self.saveMicroSleepIfNeeded(microSleep: microSleep)
+                }
 
             case .failure(_):
                 self.lock.unlock()
